9ca0ac04491050412ebfb13fc8f1631966600805
nothing628 <yogyphang@gmail.com>
2018-12-23 02:34:10 +0700

    add view

--- app/Controllers/Http/RtcController.js
+++ app/Controllers/Http/RtcController.js
@@ -1,6 +1,9 @@
 'use strict'
 
 class RtcController {
+  index ({ request, response, view }) {
+    return view.render('room.list')
+  }
 }
 
 module.exports = RtcController
--- resources/js/app.js
+++ resources/js/app.js
@@ -13,4 +13,23 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+import ReactHabitat                 from 'react-habitat';
+import SomeReactComponent           from './components/Example';
+
+class MyApp extends ReactHabitat.Bootstrapper {
+  constructor(){
+    super();
+
+    // Create a new container builder:
+    const builder = new ReactHabitat.ContainerBuilder();
+
+    // Register a component:
+    builder.register(SomeReactComponent).as('Example');
+
+    // Finally, set the container:
+    this.setContainer(builder.build());
+  }
+}
+
+// Always export a 'new' instance so it immediately evokes:
+export default new MyApp();
