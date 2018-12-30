90f509b29b938e1ceb81e9aa624aee1c1c9461a0
nothing628 <yogyphang@gmail.com>
2018-12-30 16:17:56 +0700

    finish release app.js

---
+++ webpack.config.prod.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'production',
+  entry: [
+    'babel-regenerator-runtime',
+    __dirname + '/resources/js/app.js'
+  ],
+  output: {
+    path: __dirname + '/public/js',
+    publicPath: '/public/',
+    filename: 'app.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  }
+};
