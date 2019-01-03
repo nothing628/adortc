7c260a478d08fb58a570f0cd6fade20f1a8d4767
nothing628 <yogyphang@gmail.com>
2019-01-03 15:02:55 +0700

    add candidate and file seed

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -12,6 +12,8 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const User = use('App/Models/User')
+const Candidate = use('App/Models/Candidate')
+const File = use('App/Models/File')
 
 class UserSeeder {
   async run () {
@@ -30,6 +32,21 @@ class UserSeeder {
       'Azhar Annisa',
       'Rifqy Arisa',
     ];
+    let cities = [
+      'Boston',
+      'Anchorage',
+      'Honolulu',
+      'Colorado Springs',
+      'Toledo',
+      'St. Paul',
+      'San Francisco',
+      'Miami',
+      'Laredo',
+      'Las Vegas',
+      'Baltimore',
+      'Austin',
+    ]
+
 
     for (let i = 0; i<names.length; i++) {
       const name = names[i];
@@ -39,8 +56,39 @@ class UserSeeder {
       newEmployer.password = 'test1234';
       newEmployer.email = cleanName + '@gmail.com';
       newEmployer.is_admin = i == 0
-      newEmployer.save();
+      await newEmployer.save();
+
+      if (i > 0) {
+        const city = cities[i - 1]
+        await this.createCandidate(name, newEmployer.id, city)
+        await this.createFile(name, i)
+      }
     }
+  }
+
+  async createCandidate(username, user_id, city) {
+    let candidate = new Candidate
+    candidate.fullname = username
+    candidate.user_id = user_id
+    candidate.address = city
+    candidate.gender = Math.random() > 0.5 ? 'L' : 'P'
+    candidate.birth_place = city
+    candidate.birth_date = new Date;
+
+    await candidate.save()
+  }
+
+  async createFile(username, i) {
+    let _mod = i % 6
+
+    if (_mod == 0) _mod = 6
+
+    const candidate = await Candidate.findByOrFail('fullname', username)
+    let file = new File
+    file.filename = _mod + '.pdf'
+    file.filetitle = 'CV'
+    file.candidate_id = candidate.id
+    await file.save()
   }
 }
 
